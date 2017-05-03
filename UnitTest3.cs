79dae61b98a450f7cb17396a2681aa38f431dad1
Nitin Adlok <nitin.adlok@in.abb.com>
2017-05-03 15:36:59 +0530

    Check post commit trigger

--- UnitTest3.cs
+++ UnitTest3.cs
@@ -9,7 +9,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod3()
         {
-            Console.WriteLine("This  is my third test");
+            Console.WriteLine("This is my third test - post commit check");
         }
     }
 }
